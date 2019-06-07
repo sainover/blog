43d4c9d7a1d1849a6e49d46740087db6e62caf7f
Kussainov Yerlan <Erlan.Kusainov@noveogroup.com>
2019-06-07 15:04:41 +0700

    Comments deleted

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,8 +22,6 @@ class RegistrationFormType extends AbstractType
             ->add('fullName')
             ->add('email')
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
@@ -32,7 +30,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
