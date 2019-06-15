e83fba9ae5778b6c36e4aac18d178be0c3880925
Kussainov Yerlan <Erlan.Kusainov@noveogroup.com>
2019-06-15 23:32:11 +0700

    hotfix

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,12 +11,12 @@ use App\Form\StatusType;
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
 use App\Service\ArticleService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\UserService;
 
 /**
  * @Route("/admin")
--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Service\TagService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagController extends AbstractController
